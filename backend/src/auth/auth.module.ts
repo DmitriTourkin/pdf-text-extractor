91a6777bf504c023e84825cd801bbb13132b56c4
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2026-09-26 16:37:35 +0300

    feat(back): добавить JwtStrategy и JwtAuthGuard для проверки токена
    - JwtStrategy читает JWT из httpOnly cookie, проверяет подпись и срок
    - JwtAuthGuard оборачивает passport-стратегию 'jwt' для @UseGuards
    - PassportModule + JwtStrategy зарегистрированы в AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserEntity } from './user.entity';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -19,5 +20,6 @@ import { UserEntity } from './user.entity';
   ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [JwtStrategy],
 })
 export class AuthModule {}
